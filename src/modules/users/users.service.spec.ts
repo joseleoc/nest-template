4aa2f485b405e5c55368e91347758dd800fa2377
Leonardo Contreras <joseleoc123@gmail.com>
2024-09-26 14:34:32 -0400

    feat(add-packages): Add bcrypt package to hash passwords and faker-js to mock data in testing

--- src/modules/users/users.service.spec.ts
+++ src/modules/users/users.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User, UserSchemaName } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(UserSchemaName),
+          useValue: Model<User>,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);

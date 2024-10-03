0b47de52e33421cef747b3b7e515fd0f16afd5a7
Leonardo Contreras <joseleoc123@gmail.com>
2024-10-03 12:49:43 -0400

    feat(plans): add PlansService with method to create default plans in database if they do not already exist

--- src/modules/plans/plans.service.ts
+++ src/modules/plans/plans.service.ts
@@ -1,4 +1,45 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Plan } from './schemas/plan.schema';
+import { DefaultPlans } from './plans.constants';
 
 @Injectable()
-export class PlansService {}
+export class PlansService {
+  // --------------------------------------------------------------------------------
+  // Constructor
+  // --------------------------------------------------------------------------------
+  constructor(
+    @InjectModel(Plan.name) private readonly planModel: Model<Plan>,
+  ) {}
+
+  // --------------------------------------------------------------------------------
+  // Public methods
+  // --------------------------------------------------------------------------------
+  /**
+   * This method is used to save the plans to the database for the first time only.
+   * It checks if the plans exist in the database, if not, it creates them.
+   */
+  createPlansIfNotExist(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.planModel
+        .find()
+        .then((existingPlans) => {
+          const documentsToCreate = DefaultPlans.filter(
+            (plan) =>
+              !existingPlans.some(
+                (existingPlan) => existingPlan.name === plan.name,
+              ),
+          );
+          this.planModel
+            .insertMany(documentsToCreate)
+            .then(() => {
+              resolve();
+            })
+            .catch((error) => reject(error));
+        })
+        .catch((error) => reject(error));
+    });
+  }
+}

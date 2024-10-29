c2c0b196d0fc2e021f7a5b060b0b67dd4f28d10f
Leonardo Contreras <joseleoc123@gmail.com>
2024-10-29 15:44:05 -0400

    feat(users): update change password DTO to include password validation using Zod for improved data integrity and user security

--- src/modules/users/dto/change-password.dto.ts
+++ src/modules/users/dto/change-password.dto.ts
@@ -3,9 +3,9 @@ import { z } from 'zod';
 import { createZodDto } from 'nestjs-zod';
 
 export const ChangePasswordDtoSchema = CreateUserDtoSchema.pick({
-  password: true,
   email: true,
 }).extend({
+  password: z.string().trim().min(0),
   newPassword: z.string().trim().min(6),
 });
 

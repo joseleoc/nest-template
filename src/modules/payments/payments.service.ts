b9d2ae96376530d5db083f0c0c02b9ef6cc25046
Leonardo Contreras <joseleoc123@gmail.com>
2024-11-22 11:20:20 -0400

    feat: Integrate Stripe SDK into PaymentsService and implement environment variable checks for secure setup

--- src/modules/payments/payments.service.ts
+++ src/modules/payments/payments.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import Stripe from 'stripe';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  // --------------------------------------------------------------------------------
+  // Local properties
+  // --------------------------------------------------------------------------------
+  stripe: Stripe;
+  // --------------------------------------------------------------------------------
+  // Constructor
+  // --------------------------------------------------------------------------------
+  constructor(private configService: ConfigService) {
+    this.checkEnvVariables();
+    this.setupStripe();
+  }
+
+  // --------------------------------------------------------------------------------
+  // Private methods
+  // --------------------------------------------------------------------------------
+  private checkEnvVariables(): void {
+    if (!this.configService.get('STRIPE_SECRET_KEY')) {
+      throw new Error(
+        'One or more environment variables are not set. Please check your .env file.',
+      );
+    }
+  }
+
+  private setupStripe() {
+    const stripeSecretKey: string | undefined =
+      this.configService.get('STRIPE_SECRET_KEY');
+    if (!stripeSecretKey) {
+      throw new Error(
+        'One or more environment variables are not set. Please check your .env file.',
+      );
+    }
+    this.stripe = new Stripe(stripeSecretKey);
+  }
+
+  // --------------------------------------------------------------------------------
+  // Public methods
+  // --------------------------------------------------------------------------------
+}
